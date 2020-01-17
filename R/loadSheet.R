fb08666857975c81d53ec8df69fd0f48a9bfb7e7
vlucet <VLucet@users.noreply.github.com>
2020-01-17 17:38:25 +0000

    Update loadSheet.R

--- R/loadSheet.R
+++ R/loadSheet.R
@@ -1,29 +1,26 @@
 # Loads a datasheet from csv and saves it
 # wrapper for `savedatasheet` function
 
-loadSheet <- function(sheetname, proj_or_sce, path = getwd(), params = NULL){
-
-  # Change parameters
-  if (!(is.null(params))){
-    if (is.list(params)){
+loadSheet <- function (sheetname, proj_or_sce, path = getwd(), params = NULL) 
+{
+  if (!(is.null(params))) {
+    if (is.list(params)) {
       mySheet <- as.data.frame(params)
-      # for (par in names(params)){
-      #   mySheet[[par]] <- params[[par]]
-      # }
-    } else{
+    }
+    else {
       stop("ERROR: parameters not provided as a list")
     }
-  } else {
-    mySheet <- read.csv(paste0(path,sheetname,".csv"), header = T)
   }
-
-  # Save the sheet
+  else {
+    mySheet <- read.csv(paste0(path, sheetname, ".csv"), 
+                        header = T)
+  }
   saved_message <- saveDatasheet(proj_or_sce, mySheet, sheetname)
-
-  # Check if sucessul
-  if (saved_message[1]=="saved"){
+  if (saved_message[1] == "saved") {
     print(paste0(sheetname, " Saved"))
-  } else{
-    stop("ERROR: Datasheet could not be loaded. Check path or name.")
+  }
+  else {
+    print(saved_message)
+    stop("ERROR: Datasheet could not be loaded. Check path or name, or loading order.")
   }
 }
