c2251e09914b12b43eb107d3064b7627b18ea282
VLucet <valentin.lucet@gmail.com>
2020-01-31 15:35:43 -0500

    add .csv

--- R/editDatasheet.R
+++ R/editDatasheet.R
@@ -46,6 +46,6 @@ editDatasheet <- function(datasheetName, # Name of datasheet
   }
   
   if(save_as_CSV){
-    write.csv(as.data.frame(datasheetTemp), paste0(CSV_folder,datasheetName))
+    write.csv(as.data.frame(datasheetTemp), paste0(CSV_folder,datasheetName,".csv"))
   }
 }
