b5e6d9a8b49f824b0d41a10afa91316a8a9b120b
VLucet <valentin.lucet@gmail.com>
2020-02-07 14:10:54 -0500

    attempting try catch

--- R/loadDatasheet.R
+++ R/loadDatasheet.R
@@ -9,17 +9,21 @@ loadDatasheet <- function (datasheetName, # Name of datasheet
                            datasheetFolder = getwd(), # Where csvs are saved
                            params = NULL # TODO to review if needed
 ){
+  # Check if empty parameters (allows to load from list of dataframe)
   if (!(is.null(params))) {
+    # if not, cast to df
     if (is.list(params)) {
-      mySheet <- as.data.frame(params)
-    }
+      mySheet <- as.data.frame(params)}
     else {
-      stop("ERROR: parameters not provided as a list")
-    }
+      stop("ERROR: parameters not provided as a list")}
   }
+  # Otherwise, load from folder
   else {
-    mySheet <- read.csv(paste0(datasheetFolder, datasheetName, ".csv"), 
-                        header = T)
+    tryCatch({
+      mySheet <- read.csv(paste0(datasheetFolder, datasheetName, ".csv"), 
+                          header = T)})
+  } error = function(e) {
+    print(paste0("Error: ", e, " // Datasheet file not present in ", datasheetFolder))
   }
   saved_message <- saveDatasheet(ssimObject, mySheet, datasheetName)
   if (saved_message[1] == "saved") {
